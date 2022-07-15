07164e626efcfb574d663f7b7ffccdda579fae19
Madhav Panchhiwala <madhav2panchhiwala@gmail.com>
2022-07-15 15:45:17 +0530

    minor update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div id={style.footer}>
         <h2>Next Sailors</h2>
-        <div><Link href={'/about'}><p className={style.link} >Aabout</p></Link> | <Link href={'/contactUs'}><p className={style.link}>Contact Us</p></Link> | Project Made with next js</div>
+        <div><Link href={'/about'}><p className={style.link} >About</p></Link> | <Link href={'/contactUs'}><p className={style.link}>Contact Us</p></Link> | Project Made with next js</div>
     </div>
   )
 }
