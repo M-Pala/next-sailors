076a3b2a6d6a330a85999c4d96565f4f4a82745c
Madhav Panchhiwala <madhav2panchhiwala@gmail.com>
2022-07-15 14:26:00 +0530

    release 5

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -1,38 +1,37 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
-// import style from '../../styles/SingleBlog.module.scss'
-// import { server } from '../../config'
+import style from '../../styles/SingleBlog.module.scss'
+import { server } from '../../config'
 
-// const SingleBlog = ({blogDetails : { title, subtitle, image, content, author}}) => {
-const SingleBlog = () => {
+const SingleBlog = ({blogDetails : { title, subtitle, image, content, author}}) => {
 
   return (
-    <div>SingleBlog</div>
-          
-        // <div className={style.mainContainer}>
-        //   <h1>{title}</h1>
-        //   <div className={style.imageContainer}>
-        //     <img src={image} alt={title}/>
-        //   </div>
-        //   <h4>{subtitle}</h4>
-        //   <br/>
-        //   <br/>
-        //   <pre>{content}</pre>
-        //   <h5>Author : <em>{author}</em></h5>
-        // </div>
+    <>      
+        <div className={style.mainContainer}>
+          <h1>{title}</h1>
+          <div className={style.imageContainer}>
+            <img src={image} alt={title}/>
+          </div>
+          <h4>{subtitle}</h4>
+          <br/>
+          <br/>
+          <pre>{content}</pre>
+          <h5>Author : <em>{author}</em></h5>
+        </div>
 
+    </>
   )
 }
 
 export default SingleBlog
 
-// export async function getServerSideProps({params:{blogId}}){
-//   const response = await fetch(`${server}/api/blogs/${blogId}`)
+export async function getServerSideProps({params:{blogId}}){
+  const response = await fetch(`${server}/api/blogs/${blogId}`)
 
-//   const data = await response.json()
-//   return{
-//       props : {
-//           blogDetails : data
-//       }
-//   }
-// }
+  const data = await response.json()
+  return{
+      props : {
+          blogDetails : data
+      }
+  }
+}
